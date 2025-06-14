0448d71b2cf2642171e371f452eabe1794dcaab9
Dorian Turner <dorianemmanuelturner@gmail.com>
2025-06-14 01:28:49 +0100

    Style: Removed unecessary options in hyprland Hjem config

--- system/wayland/hyprlandconf.nix
+++ system/wayland/hyprlandconf.nix
@@ -1,9 +1,7 @@
 {
 	hjem.users.dorian.files = {
 		".config/hypr/hyprland.conf" = {
-			enable = true;
 			target = ".config/hypr/hyprland.conf";
-			text = builtins.readFile ./hyprland.conf;
 		};
 	};
 }
